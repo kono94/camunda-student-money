a36696da790cda5a9aeee98ed8bd9869aee33d7c
Jan Löwenstrom <jan.loewenstrom@web.de>
2021-03-17 15:33:39 +0100

    create users, groups and filter after deleting default setup after deployment

--- src/main/java/net/lwenstrom/CamundaBpmProcessApplication.java
+++ src/main/java/net/lwenstrom/CamundaBpmProcessApplication.java
@@ -1,11 +1,16 @@
 package net.lwenstrom;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.logging.Logger;
+
+import net.lwenstrom.util.AuthorizationManager;
 import org.camunda.bpm.application.PostDeploy;
 import org.camunda.bpm.application.ProcessApplication;
 import org.camunda.bpm.application.impl.ServletProcessApplication;
+
 import org.camunda.bpm.engine.ProcessEngine;
+import static org.camunda.bpm.engine.authorization.Permissions.*;
+
 
 /**
  * Process Application exposing this application's resources the process engine.
@@ -13,21 +18,76 @@ import org.camunda.bpm.engine.ProcessEngine;
 @ProcessApplication
 public class CamundaBpmProcessApplication extends ServletProcessApplication {
 
+    private final Logger LOGGER = Logger.getLogger(CamundaBpmProcessApplication.class.getName());
 
-  /**
-   * In a @PostDeploy Hook you can interact with the process engine and access
-   * the processes the application has deployed.
-   *
-   * docker run -d --name camunda -it -p 8080:8080 -e PREPEND_JAVA_OPTS='-Dfile.encoding=UTF-8' camunda/camunda-bpm-platform:latest
-   */
-  @PostDeploy
-  public void onDeploymentFinished(ProcessEngine processEngine) {
+    /**
+     * In a @PostDeploy Hook you can interact with the process engine and access
+     * the processes the application has deployed.
+     * <p>
+     * docker run -d --name camunda -it -p 8080:8080 -e PREPEND_JAVA_OPTS='-Dfile.encoding=UTF-8' camunda/camunda-bpm-platform:latest
+     */
+    @PostDeploy
+    public void onDeploymentFinished(ProcessEngine engine) {
+        LOGGER.info("Application has been deployed");
+        AuthorizationManager am = new AuthorizationManager(engine);
 
-    // start an initial process instance
+        // Delete default users and groups
+        am.deleteUser("john");
+        am.deleteUser("peter");
+        am.deleteUser("mary");
+        am.deleteGroup("accounting");
+        am.deleteGroup("management");
+        am.deleteGroup("sales");
+
+        am.deleteFilter("Mary's Tasks");
+        am.deleteFilter("Peter's Tasks");
+        am.deleteFilter("John's Tasks");
+        am.deleteFilter("Accounting");
+
+
+        // Create users
+        am.createNewUser("judith", "Judith", "Moller", "judith", "jmoller@asta.hs-bremerhaven.de");
+        am.createNewUser("alexander", "Alexander", "Schulz", "alexander", "aschulz@iup.hs-bremerhaven.de");
+        am.createNewUser("sabine", "Sabine", "Kreesmann", "sabine", "skreesmann@stadt-bremerhaven.de");
+        am.createNewUser("christian", "Christian", "Erdnuss", "christian", "cerdnuss@stadt-bremerhaven.de");
+
+
+        // Create groups (AStA, IuP and city)
+        am.createNewGroup("asta", "AStA");
+        am.createNewGroup("iup", "IuP");
+        am.createNewGroup("city", "Stadt Bremerhaven");
+
+
+        //  Add users to respective groups
+        am.createMembership("judith", "asta");
+        am.createMembership("alexander", "iup");
+        am.createMembership("sabine", "city");
+        am.createMembership("christian", "city");
+
+
+        // authorize groups for tasklist only:
+        am.setTasklistAuthorizationForGroup("asta", ProcessConstants.PROCESS_DEFINITION_KEY, READ, READ_HISTORY);
+        am.setTasklistAuthorizationForGroup("iup", ProcessConstants.PROCESS_DEFINITION_KEY, READ, READ_HISTORY);
+        am.setTasklistAuthorizationForGroup("city", ProcessConstants.PROCESS_DEFINITION_KEY, READ, READ_HISTORY);
+
+
+        // set user permissions in group
+        am.setUserPermissionInGroup("asta", "judith", READ);
+        am.setUserPermissionInGroup("iup", "alexander", READ);
+        am.setUserPermissionInGroup("city", "sabine", READ);
+        am.setUserPermissionInGroup("city", "christian", READ);
+
+
+        // create default filters
+        am.createGroupFilter("Aufgaben für Gruppe AStA", -3, "asta", "AStA", "demo", READ);
+        am.createGroupFilter("Aufgaben für Gruppe IuP", -3, "iup", "IuP", "demo", READ);
+        am.createGroupFilter("Aufgaben für Gruppe Stadt", -3, "city", "Stadt Bremerhaven", "demo", READ);
+
+
+        // start an initial process instance
 //    Map<String, Object> variables = new HashMap<String, Object>();
 //    variables.put("name", "John");
 //
 //    processEngine.getRuntimeService().startProcessInstanceByKey(PROCESS_DEFINITION_KEY, variables);
-  }
-
+    }
 }
---
+++ src/main/java/net/lwenstrom/util/AuthorizationManager.java
@@ -0,0 +1,133 @@
+package net.lwenstrom.util;
+
+import net.lwenstrom.CamundaBpmProcessApplication;
+import org.camunda.bpm.engine.AuthorizationService;
+import org.camunda.bpm.engine.FilterService;
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.TaskService;
+import org.camunda.bpm.engine.authorization.Authorization;
+import org.camunda.bpm.engine.authorization.Permissions;
+import org.camunda.bpm.engine.authorization.Resources;
+import org.camunda.bpm.engine.filter.Filter;
+import org.camunda.bpm.engine.identity.Group;
+import org.camunda.bpm.engine.identity.User;
+import org.camunda.bpm.engine.impl.IdentityServiceImpl;
+import org.camunda.bpm.engine.task.TaskQuery;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+
+import static org.camunda.bpm.engine.authorization.Authorization.AUTH_TYPE_GRANT;
+import static org.camunda.bpm.engine.authorization.Permissions.ACCESS;
+import static org.camunda.bpm.engine.authorization.Resources.*;
+
+public class AuthorizationManager {
+    private final Logger LOGGER = Logger.getLogger(AuthorizationManager.class.getName());
+
+    final IdentityServiceImpl identityService;
+    final AuthorizationService authorizationService;
+    final FilterService filterService;
+    final TaskService taskService;
+
+    public AuthorizationManager(ProcessEngine engine) {
+        identityService = (IdentityServiceImpl) engine.getIdentityService();
+        authorizationService = engine.getAuthorizationService();
+        filterService = engine.getFilterService();
+        taskService = engine.getTaskService();
+    }
+
+    public void deleteUser(String userId){
+        identityService.deleteUser("john");
+    }
+
+    public void deleteGroup(String groupId){
+        identityService.deleteGroup(groupId);
+    }
+
+    public void deleteFilter(String filterName){
+        Filter filter = filterService.createFilterQuery().filterName(filterName).singleResult();
+        filterService.deleteFilter(filter.getId());
+    }
+
+    public void createMembership(String groupId, String userId){
+        identityService.createMembership(groupId, userId);
+    }
+    public void createNewUser(String userId,
+                              String firstName,
+                              String lastName,
+                              String password,
+                              String email) {
+        User user = identityService.newUser(userId);
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        user.setPassword(password);
+        user.setEmail(email);
+        identityService.saveUser(user);
+    }
+
+    public void createNewGroup(String groupId, String name) {
+        Group astaGroup = identityService.newGroup(groupId);
+        astaGroup.setName(name);
+        astaGroup.setType("WORKFLOW");
+        identityService.saveGroup(astaGroup);
+    }
+
+    public void setTasklistAuthorizationForGroup(String groupId, String processDefinition, Permissions... permissions) {
+        Authorization tasklistAuth = authorizationService.createNewAuthorization(AUTH_TYPE_GRANT);
+        tasklistAuth.setGroupId(groupId);
+        tasklistAuth.addPermission(ACCESS);
+        tasklistAuth.setResourceId("tasklist");
+        tasklistAuth.setResource(APPLICATION);
+        authorizationService.saveAuthorization(tasklistAuth);
+
+        Authorization readProcessDefinition = authorizationService.createNewAuthorization(AUTH_TYPE_GRANT);
+        readProcessDefinition.setGroupId(groupId);
+        for (Permissions p : permissions) {
+            readProcessDefinition.addPermission(p);
+        }
+        readProcessDefinition.setResource(Resources.PROCESS_DEFINITION);
+        // restrict to invoice process definition only
+        readProcessDefinition.setResourceId(processDefinition);
+        authorizationService.saveAuthorization(readProcessDefinition);
+    }
+
+    public void setUserPermissionInGroup(String groupId, String resourceId, Permissions... permissions) {
+        Authorization authorization = authorizationService.createNewAuthorization(AUTH_TYPE_GRANT);
+        authorization.setGroupId(groupId);
+        authorization.setResource(USER);
+        authorization.setResourceId(resourceId);
+        for (Permissions p : permissions) {
+            authorization.addPermission(p);
+        }
+       authorizationService.saveAuthorization(authorization);
+    }
+
+
+    public void createGroupFilter(String description,
+                                  int priority,
+                                  String groupId,
+                                  String filterName,
+                                  String owner,
+                                  Permissions... permissions) {
+        Map<String, Object> filterProperties = new HashMap<>();
+        filterProperties.put("description", description);
+        filterProperties.put("priority", priority);
+        // addVariables(filterProperties);
+        TaskQuery query = taskService.createTaskQuery().taskCandidateGroupIn(Arrays.asList(groupId)).taskUnassigned();
+        Filter candidateGroupTasksFilter = filterService.newTaskFilter().setName(filterName).setProperties(filterProperties).setOwner(owner).setQuery(query);
+        filterService.saveFilter(candidateGroupTasksFilter);
+
+        Authorization managementGroupFilterRead = authorizationService.createNewAuthorization(Authorization.AUTH_TYPE_GRANT);
+        managementGroupFilterRead.setResource(FILTER);
+        managementGroupFilterRead.setResourceId(candidateGroupTasksFilter.getId());
+        for (Permissions p : permissions) {
+            managementGroupFilterRead.addPermission(p);
+        }
+        managementGroupFilterRead.setGroupId(groupId);
+        authorizationService.saveAuthorization(managementGroupFilterRead);
+
+    }
+}
